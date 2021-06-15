37b1c9d35dc14270ed9f42cb12e4943ee22e6d2d
NilsS <Nils.Steinz@hotmail.com>
2021-06-15 11:53:15 +0200

    fixed some issues
    Signed-off-by: NilsS <Nils.Steinz@hotmail.com>

--- test/test_cpm.py
+++ test/test_cpm.py
@@ -38,9 +38,6 @@ class test_norm(unittest.TestCase):
 
 
     def test_set_factor(self):
-        """
-
-        """
         self.assertEqual(self.Norm.factor, None)
         data = pd.DataFrame([10, 10, 10, 10])
         self.Norm.set_factor(data)
@@ -53,21 +50,16 @@ class test_norm(unittest.TestCase):
 
     def test_lib_size_cut_off_correct(self):
         data = pd.DataFrame(np.arange(0, 10))
-        target_4_6 = norm().cpm(data.iloc[4:6])
+        target_4_6 = self.Norm.cpm(data.iloc[4:6])
         testing.assert_frame_equal(
             norm().cpm(data, lib_size=slice(4, 6)), target_4_6, check_dtype=False
         )
 
     def test_lib_size_raise_error_if_empty(self):
-        """
-        test if the __lib_select functions raises a error if you have less than 2 indexes.
-        CPM should not return a thing if you only have a single row.
-        :return:
-        """
         data = pd.DataFrame(np.arange(0, 10))
         with self.assertRaises(Exception) as e:
             norm().cpm(data, lib_size=slice(7, 2))
-        self.assertTrue(type(e.exception) in [ArithmeticError])
+        self.assertIsInstance(e.exception, (ArithmeticError,))
 
     def test_CPM_not_norm(self):
         data = self.main_data.copy()
